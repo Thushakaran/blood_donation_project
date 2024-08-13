485c68c615b0d20dd0c5c4190317ddee83fd5c21
Thushakaran <tthushakaran19@gmail.com>
2024-08-13 20:39:07 +0530

    about

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="Stylesheet"  href="about.css">
+</head>
+<body>
+    <div id="container">
+        
+            <nav>
+                <h3 class="navigation">Blood_Share</h3>
+                <ul>
+                    <li> 🏠Home | </li>
+                    <li> 🔎About Us | </li>
+                    <li> 📼Services | </li>
+                    <li> 📞Contact Us | </li>
+                    <li> 👨‍✈️Admin</li>
+                </ul>
+                <div class="menu">
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                </div>
+            </nav>
+    </div>
+   
+        <div class="heading">
+                <h1>About<p>|Blood Bank</p></h1>
+                  
+        </div>
+        <div class=pos_sen>
+        <div class="image01">
+            <img src="img_01.png" alt="image01" class="im01">
+            
+        </div>
+        
+        
+        <div class="box">
+            
+                
+                <p>Our blood bank is a cornerstone of lifesaving care, committed to providing a continuous and safe supply of blood and blood products. We collect, test, and store donations with the utmost care, ensuring that blood is always available to those in need. From red blood cells to plasma and platelets, our advanced facilities prepare and distribute these essential components, supporting a wide range of medical treatments. We invite you to join us in this vital mission—donate today and be the lifeline someone needs tomorrow.</p>
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
