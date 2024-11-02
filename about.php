2b715f82b376b64a5f173c0fc1f84720f3a5ac6b
Thushakaran <tthushakaran19@gmail.com>
2024-11-02 18:58:38 +0530

    Code Updated

--- about.php
+++ about.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="Stylesheet"  href="CSS/about.css">
+    <link rel="stylesheet" href="CSS/about.css">
     <link rel="icon" href="images/icon 03.jpg">
 </head>
+
 <body>
-    <?php
-        include("navigation_bar.php");
-    ?>
+    <?php include("navigation_bar.php"); ?>
+    
+    <div class="heading">
+        <h1>About Us</h1>
+    </div>
+    
     <div class="container">
-        <div class="heading">
-            <h1>About<p>|Blood Bank</p></h1>               
-        </div>
-        <div class=pos_sen>
+        <div class="content">
             <div class="image01">
-                <img src="images/img_01.png" alt="image01" class="im01">              
-            </div>       
+                <img src="images/img_01.png" alt="image01" class="im01">
+            </div>
             <div class="box">
                 <p>Our blood bank is a cornerstone of lifesaving care, committed to providing a continuous and safe supply of blood and blood products. We collect, test, and store donations with the utmost care, ensuring that blood is always available to those in need. From red blood cells to plasma and platelets, our advanced facilities prepare and distribute these essential components, supporting a wide range of medical treatments. We invite you to join us in this vital mission—donate today and be the lifeline someone needs tomorrow.</p>
             </div>
-            <?php
-            include("footer.php");
-            ?>
         </div>
     </div>
+    
+    <?php include("footer.php"); ?>
 </body>
+
 </html>
