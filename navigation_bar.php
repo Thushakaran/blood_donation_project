cc2dbbab423475f886894b45de7fa42a3e91aee9
KeerthiGopalavannan <keerthigopalm@gmail.com>
2024-11-16 00:10:51 +0530

    add

--- navigation_bar.php
+++ navigation_bar.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="CSS/navigation_bar.css">
+    
+    <link rel="icon" href="images/blood drop.png";
 </head>
 <body>
     <div class="navbar">
