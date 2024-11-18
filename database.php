1979e276d570472d0f2360059c64cf3a184b55a0
Thushakaran <tthushakaran19@gmail.com>
2024-11-18 18:31:28 +0530

    database

--- database.php
+++ database.php
@@ -1,12 +1,11 @@
 <?php
-    $host="localhost:3307";
-    $user="root";
-    $password="";
-    $db_name="blood_donation";
+$host = "localhost";
+$user = "root";
+$password = "";
+$db_name = "blood_donation";
 
-    $db=mysqli_connect($host,$user,$password,$db_name);
+$db = mysqli_connect($host, $user, $password, $db_name);
 
-    if($db===false){
-        die("Connection Error");
-    }
-?>
+if ($db === false) {
+    die("Connection Error");
+}
